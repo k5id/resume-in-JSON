19d62c93414c3c203b25867b7dfb5f6887b09b13
k5id <48526324+k5id@users.noreply.github.com>
2022-05-25 01:16:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let JsonResume={
+    "Title": {
+      "name": "Marcus Aurelius",
+      "email": "MarcusAurelius@gmail.com",
+      "phone": +91889587456,
+        
+    },
+    "Education": {
+        "Institution": "Roman School of Engineering",
+        "Stream": "Civil",
+        "GPA": "9.9"
+      },
+    "work": [
+      {
+        "company": "Google GmbH",
+        "position": "Senior Customer Service Representative",
+        "Period": "Sep ’2019 - Current",
+        "Summary Of work": "Handle 400+ weekly customer inquiries via chat and email for 6 regions including UK and Australia",
+
+        "company2": "Microsoft GmbH",
+        "position2": "Customer Service Representative",
+        "Period2": "Sep ’2018 - 2019",
+        "Summary Of work2": "Handle weekly customer inquiries via chat and email for 6 regions including UK and Australia",
+      },
+    ],
+    "Skills": [
+      {
+        "1": "Focused on customer Satisfaction",
+        "2": "Customer Support Expert",
+        "3 ": "Zendesk CRM / Adyen / Google Suite",
+        "gpa": "Social Media",
+      }
+    ],
+    "skills": [
+      {
+        "name": "java,HTML,CSS,JS",
+        "level": "beginner",
+      }
+    ],
+   
+  }
+  console.log(JsonResume);
